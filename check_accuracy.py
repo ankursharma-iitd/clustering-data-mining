c6bf7de7d2aa8f23073aa01c39a29657074cd313
ankursharma-iitd <ankursharma.iitd@gmail.com>
2018-09-18 21:57:53 +0530

    updated check

--- check_accuracy.py
+++ check_accuracy.py
@@ -2,18 +2,20 @@
 # param-1 : actual label file name
 # param-2 : predicted label file name
 
-from sklearn.metrics.cluster import v_measure_score
-from sklearn.metrics.cluster import completeness_score
-from sklearn.metrics.cluster import homogeneity_score
+from sklearn import metrics
 import numpy as np
 import sys
 
 actual_labels = str(sys.argv[1])
 my_labels = str(sys.argv[2])
 
-true_labels = np.loadtxt(actual_labels)
-predicted_labels = np.loadtxt(my_labels)
+labels_true = np.loadtxt(actual_labels)
+labels = np.loadtxt(my_labels)
 
-print('COMPLETENESS SCORE : ' + str(completeness_score(true_labels, predicted_labels)))
-print('HOMOGENEITY SCORE : ' + str(homogeneity_score(true_labels, predicted_labels)))
-print('V_MEASURE SCORE : ' + str(v_measure_score(true_labels, predicted_labels)))
+print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(
+    labels_true, labels))
+print("Adjusted Mutual Information: %0.3f" %
+      metrics.adjusted_mutual_info_score(labels_true, labels))
