ecdd7b757358c14a9f1f18bffc024b159b079059
Kartikeya Sharma <karthiksharma98@users.noreply.github.com>
2018-09-21 15:57:21 +0530

    Submission

--- 2015CS50278.sh
+++ 2015CS50278.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ "$2" = "-kmeans" ]; then
-	./kmeans $3 $4
-elif [ "$2" = "-dbscan" ]; then
- 	./dbscan $3 $4 $5
-elif [ "$2" = "-optics" ]; then
-	python3 optics.py $5 $4 $3
+if [ "$1" = "-kmeans" ]; then
+	./kmeans $2 $3
+elif [ "$1" = "-dbscan" ]; then
+ 	./dbscan $2 $3 $4
+elif [ "$1" = "-optics" ]; then
+	python3 optics.py $4 $3 $2
 else
 	echo "Incorrect usage"
 fi
